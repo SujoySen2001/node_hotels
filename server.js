38fe9b86a30ce4ac699962386eba72efdf7a4c0d
Sujoy <sujoysen2001@gmail.com>
2024-09-30 13:22:02 +0530

    Shift MongoDB to Online Server

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 
 const db=require('./db');
 
+require('dotenv').config()
 const bodyParser= require('body-parser');
 app.use(bodyParser.json()); // req.body
+
+const PORT=process.env.PORT || 3000 
 
 app.get('/', function (req, res) {
   res.send('Welcome to my hotel....')
@@ -18,6 +21,6 @@ const menuRountes=require('./routes/menuRounter')
 app.use('/person',personRountes)
 app.use('/menu',menuRountes)
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
   console.log('Listening on port 3000');
 })
